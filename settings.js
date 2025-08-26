943b05088b5ccd1e6adb8bebdccae1537080c316
SalimIwiqja <salimemaataoui@gmail.com>
2025-08-26 07:21:31 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
 const settings = {
-  packname: 'sᴛᴀʀ xᴅ',
-  author: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ',
-  botName: 'sᴛᴀʀ xᴅ', //Your bot name
+  packname: 'aeson-md',
+  author: 'salim',
+  botName: 'aeson-md', //Your bot name
   version: '1.2.0',
-  botOwner: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //Your name
-  imageUrl: 'https://i.ibb.co/rRg9wTZV/malvin-xd.jpg',    
-  ownerNumber: process.env.OWNER_NUMBER || '263714757857',
+  botOwner: 'salim', //Your name
+  imageUrl: 'https://i.ibb.co/Lhq9CYP7/125265684b09c7e1dc6ae0d7e0ded4aa.jpg',    
+  ownerNumber: process.env.OWNER_NUMBER || '212605158422',
   giphyApiKey: process.env.GIPHY_API_KEY || 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: 'public',
   description: 'This is a bot for managing group commands and automating tasks.',
